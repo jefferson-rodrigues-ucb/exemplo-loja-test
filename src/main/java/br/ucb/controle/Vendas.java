4e5a36fa98605ce3fe999e4c21783f167f4fc2bc
Jefferson Rodrigues UCB <jefferson.rodrigues@p.ucb.br>
2025-04-01 11:11:14 -0300

    Aula sobre before, after e HU

--- src/main/java/br/ucb/controle/Vendas.java
+++ src/main/java/br/ucb/controle/Vendas.java
@@ -1,5 +1,12 @@
 package main.java.br.ucb.controle;
 
+import main.java.br.ucb.entidade.Pedido;
+
 public class Vendas {
     // TODO criar vendas
+
+    public static float venda(Pedido pedido) {
+        System.out.println(pedido);
+        return pedido.calcularValorTotal();
+    }
 }
